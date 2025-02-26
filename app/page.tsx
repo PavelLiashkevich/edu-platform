030bcd72f17e9f67193b5bac67408858d65db7b1
pavel <paul8paul8@mail.ru>
2025-02-26 15:34:01 +0300

    init: start edu platform

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div>
+        <div className="bg-background text-foreground">
+            <h1 className="text-4xl font-bold">Привет, мир!</h1>
+        </div>
+    </div>
+  );
+}
